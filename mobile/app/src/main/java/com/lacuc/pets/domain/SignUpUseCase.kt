cadd45f2771d928a50e0c6d5c663770923ac4127
SeongHwan Kim <shshksh58@gmail.com>
2021-03-28 22:19:46 +0900

    [test] 새로운 사용자 정보에 대한 회원가입 테스트
    - 시스템에 등록되지 않은 사용자 정보로 가입을 요청하는 해피 패스 테스트.
    인터페이스와 유스케이스에 컴파일을 위한 함수 선언을 추가함.

--- mobile/app/src/main/java/com/lacuc/pets/domain/SignUpUseCase.kt
+++ mobile/app/src/main/java/com/lacuc/pets/domain/SignUpUseCase.kt
@@ -3,4 +3,7 @@ package com.lacuc.pets.domain
 import com.lacuc.pets.data.LoginService
 
 class SignUpUseCase(val loginService: LoginService) {
+    operator fun invoke(name: String, email: String, password: String): Boolean {
+        TODO("Not yet implemented")
+    }
 }
--- mobile/app/src/test/java/com/lacuc/pets/SignUpUseCaseTest.kt
+++ mobile/app/src/test/java/com/lacuc/pets/SignUpUseCaseTest.kt
@@ -2,10 +2,12 @@ package com.lacuc.pets
 
 import com.lacuc.pets.data.LoginService
 import com.lacuc.pets.domain.SignUpUseCase
+import org.junit.Assert.assertTrue
 import org.junit.Before
+import org.junit.Test
 import org.junit.runner.RunWith
 import org.mockito.Mock
-import org.mockito.Mockito.mock
+import org.mockito.Mockito.*
 import org.mockito.junit.MockitoJUnitRunner
 
 @RunWith(MockitoJUnitRunner::class)
@@ -20,6 +22,21 @@ class SignUpUseCaseTest {
     fun init_UseCase_With_MockService() {
         loginService = mock(LoginService::class.java)
         useCase = SignUpUseCase(loginService)
+    }
+
+    @Test
+    fun signUp_newEmail() {
+        val param = mapOf(
+            "name" to "John",
+            "email" to "newUser@lacuc.com",
+            "password" to "password"
+        )
+        `when`(loginService.signUp(param)).thenReturn(true)
+
+        val result = useCase("John", "newUser@lacuc.com", "password")
+
+        verify(loginService).signUp(param)
+        assertTrue(result)
     }
 
 }
