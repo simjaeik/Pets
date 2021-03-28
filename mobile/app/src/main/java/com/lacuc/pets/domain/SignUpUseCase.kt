435c03c2238fc1da4619583ce7bd3b32e42e0256
SeongHwan Kim <shshksh58@gmail.com>
2021-03-28 22:34:23 +0900

    [feat] 회원가입 유스케이스 구현
    - 테스트 통과를 위해 로그인 서비스에 파라미터로 받은 유저 데이터로
    회원가입을 요청함.

--- mobile/app/src/main/java/com/lacuc/pets/domain/SignUpUseCase.kt
+++ mobile/app/src/main/java/com/lacuc/pets/domain/SignUpUseCase.kt
@@ -2,8 +2,8 @@ package com.lacuc.pets.domain
 
 import com.lacuc.pets.data.LoginService
 
-class SignUpUseCase(val loginService: LoginService) {
+class SignUpUseCase(private val loginService: LoginService) {
     operator fun invoke(name: String, email: String, password: String): Boolean {
-        TODO("Not yet implemented")
+        return loginService.signUp(mapOf("name" to name, "email" to email, "password" to password))
     }
 }
