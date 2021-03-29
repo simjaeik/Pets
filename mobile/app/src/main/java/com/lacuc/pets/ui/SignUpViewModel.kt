2bb833dc4fc1ee5a22da76b8504e111deb2e42f1
SeongHwan Kim <shshksh58@gmail.com>
2021-03-29 22:22:54 +0900

    [test] 동일한 패스워드 입력에 대한 버튼 활성화 여부 테스트
    - 동일한 패스워드 입력에 대해 버튼 활성화 여부를 변경하는 테스트 작성.
    라이브 데이터의 테스트를 위해 InstantTaskExecutorRule 을 추가함.

--- mobile/app/src/main/java/com/lacuc/pets/ui/SignUpViewModel.kt
+++ mobile/app/src/main/java/com/lacuc/pets/ui/SignUpViewModel.kt
@@ -1,7 +1,18 @@
 package com.lacuc.pets.ui
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.lacuc.pets.domain.SignUpUseCase
+import io.reactivex.rxjava3.core.ObservableSource
 
 class SignUpViewModel(private val signUpUseCase: SignUpUseCase) : ViewModel() {
+
+    val completeBtnEnable = MutableLiveData<Boolean>()
+
+    fun isPasswordEqual(
+        password: ObservableSource<String>,
+        passwordRepeat: ObservableSource<String>
+    ) {
+
+    }
 }
--- mobile/app/src/test/java/com/lacuc/pets/SignUpViewModelTest.kt
+++ mobile/app/src/test/java/com/lacuc/pets/SignUpViewModelTest.kt
@@ -1,20 +1,44 @@
 package com.lacuc.pets
 
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.lacuc.pets.LiveDataTestUtil.getOrAwaitValue
 import com.lacuc.pets.data.LoginService
 import com.lacuc.pets.domain.SignUpUseCase
 import com.lacuc.pets.ui.SignUpViewModel
+import io.reactivex.rxjava3.core.Observable
+import org.junit.Assert.assertTrue
 import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
 import org.mockito.Mock
+import org.mockito.Mockito.mock
 
 class SignUpViewModelTest {
 
-    lateinit var viewModel: SignUpViewModel
+    private lateinit var viewModel: SignUpViewModel
 
     @Mock
-    lateinit var loginService: LoginService
+    private lateinit var loginService: LoginService
+
+    // Executes each task synchronously using Architecture Components.
+    @get:Rule
+    var instantExecutorRule = InstantTaskExecutorRule()
 
     @Before
     fun init_viewModel() {
+        loginService = mock(LoginService::class.java)
         viewModel = SignUpViewModel(SignUpUseCase(loginService))
+    }
+
+    @Test
+    fun passwordEqualCheck_samePassword() {
+        viewModel.completeBtnEnable.value = false
+
+        viewModel.isPasswordEqual(
+            Observable.just("password"),
+            Observable.just("password")
+        )
+
+        assertTrue(viewModel.completeBtnEnable.getOrAwaitValue())
     }
 }
