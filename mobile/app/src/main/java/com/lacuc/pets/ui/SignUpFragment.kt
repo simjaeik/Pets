dcebbfebdd93172b3fe3bfc89795385133bb146e
SeongHwan Kim <shshksh58@gmail.com>
2021-03-30 19:09:38 +0900

    [feat] 데이터 바인딩과 UI 업데이트 적용
    - 데이터 바인딩을 적용하여 레이아웃의 vm 변수를 설정하고 뷰가 입력값에 맞게
    상태를 변경할 수 있도록 구현함.

--- mobile/app/src/main/java/com/lacuc/pets/ui/SignUpFragment.kt
+++ mobile/app/src/main/java/com/lacuc/pets/ui/SignUpFragment.kt
@@ -5,15 +5,23 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupWithNavController
+import com.jakewharton.rxbinding4.widget.textChanges
+import com.lacuc.pets.ViewModelFactory
 import com.lacuc.pets.databinding.FragmentSignUpBinding
+import io.reactivex.rxjava3.disposables.CompositeDisposable
 
 
 class SignUpFragment : Fragment() {
     private var _binding: FragmentSignUpBinding? = null
     private val binding get() = _binding!!
+
+    private val viewModel: SignUpViewModel by viewModels { ViewModelFactory() }
+
+    private val disposables = CompositeDisposable()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -27,11 +35,35 @@ class SignUpFragment : Fragment() {
 
         binding.toolbar.setupWithNavController(navController, appBarConfiguration)
 
+        binding.vm = viewModel
+        binding.lifecycleOwner = viewLifecycleOwner
+
+        disposables.add(
+            viewModel.bindPasswordConfirmError(
+                binding.textInputPassword.textChanges(),
+                binding.textInputPasswordConfirm.textChanges()
+            )
+        )
+
+        disposables.add(
+            viewModel.bindCompleteBtnEnable(
+                binding.textInputName.textChanges(),
+                binding.textInputEmail.textChanges(),
+                binding.textInputPassword.textChanges(),
+                binding.textInputPasswordConfirm.textChanges()
+            )
+        )
+
+        viewModel.passwordConfirmError.observe(viewLifecycleOwner) {
+            binding.inputLayoutPasswordConfirm.error = it
+        }
+
         return binding.root
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
+        disposables.dispose()
         _binding = null
     }
 }
