b4fab3f707191ffb760a029ffe5c2361d211b9b5
SeongHwan Kim <shshksh58@gmail.com>
2021-03-31 22:05:37 +0900

    [feat] 로그인 화면 내비게이션 설정
    - 내비게이션 컴포넌트를 사용하여 로그인 -> 회원가입 화면 전환과 애니메이션
    설정.

--- mobile/app/src/main/java/com/lacuc/pets/ui/SignInFragment.kt
+++ mobile/app/src/main/java/com/lacuc/pets/ui/SignInFragment.kt
@@ -5,11 +5,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
+import androidx.navigation.fragment.findNavController
 import com.lacuc.pets.databinding.FragmentSignInBinding
 
 class SignInFragment : Fragment() {
     private var _binding: FragmentSignInBinding? = null
     private val binding get() = _binding!!
+
+    private lateinit var navController:  NavController
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -17,7 +21,16 @@ class SignInFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentSignInBinding.inflate(inflater, container, false)
+        navController = findNavController()
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.btnSignUp.setOnClickListener {
+            val action = SignInFragmentDirections.actionSignInFragmentToSignUpFragment()
+            navController.navigate(action)
+        }
     }
 
     override fun onDestroyView() {
