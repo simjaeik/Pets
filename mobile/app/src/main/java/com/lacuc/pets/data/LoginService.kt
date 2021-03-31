230d304cec29ccc5900803337e1516fcb57f3753
SeongHwan Kim <shshksh58@gmail.com>
2021-03-31 22:07:41 +0900

    [bug] 화면 전환 트랜지션 버그 수정
    - 화면 전환 시 PopEnterAnim이 정상적으로 동작하지 않았는데 라이브러리 버전이
    잘못 설정된 것을 확인하여 수정함. 추가로 실행에 문제가 없도록 로그인 서비스
    인터페이스의 함수를 TODO 처리함.

--- mobile/app/src/main/java/com/lacuc/pets/data/LoginService.kt
+++ mobile/app/src/main/java/com/lacuc/pets/data/LoginService.kt
@@ -1,7 +1,11 @@
 package com.lacuc.pets.data
 
 interface LoginService {
-    fun signUp(param: Map<String, String>): Boolean
+    fun signUp(param: Map<String, String>): Boolean {
+        TODO("Not yet implemented")
+    }
 
-    fun signIn(param: Map<String, String>): Boolean
+    fun signIn(param: Map<String, String>): Boolean {
+        TODO("Not yet implemented")
+    }
 }
