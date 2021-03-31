6b364d6a8ba004965418b290393889e4b4439b49
SeongHwan Kim <shshksh58@gmail.com>
2021-03-31 14:31:46 +0900

    [test] 로그인 유스케이스 테스트 작성
    - 저장된 계정 정보로의 로그인 시도외 존재하지 않는 정보로의 로그인 시도를
    테스트하는 함수 작성.

--- mobile/app/src/main/java/com/lacuc/pets/domain/SignInUseCase.kt
+++ mobile/app/src/main/java/com/lacuc/pets/domain/SignInUseCase.kt
@@ -3,4 +3,7 @@ package com.lacuc.pets.domain
 import com.lacuc.pets.data.LoginService
 
 class SignInUseCase(private val loginService: LoginService) {
+    operator fun invoke(email: String, password: String): Boolean {
+        TODO()
+    }
 }
--- mobile/app/src/test/java/com/lacuc/pets/SignInUseCaseTest.kt
+++ mobile/app/src/test/java/com/lacuc/pets/SignInUseCaseTest.kt
@@ -2,7 +2,9 @@ package com.lacuc.pets
 
 import com.lacuc.pets.data.LoginService
 import com.lacuc.pets.domain.SignInUseCase
+import org.junit.Assert
 import org.junit.Before
+import org.junit.Test
 import org.junit.runner.RunWith
 import org.mockito.Mock
 import org.mockito.Mockito
@@ -20,6 +22,22 @@ class SignInUseCaseTest {
     fun init_UseCase_With_MockService() {
         loginService = Mockito.mock(LoginService::class.java)
         useCase = SignInUseCase(loginService)
+    }
+
+    @Test
+    fun signIn_existentUser() {
+        val result = useCase("oldUser@lacuc.com", "password")
+
+        Assert.assertTrue(result)
+        Mockito.verify(loginService).signIn(Mockito.anyMap())
+    }
+
+    @Test
+    fun signIn_nonexistentUser() {
+        Assert.assertFalse(useCase("newUser1@lacuc.com", "password"))
+        Assert.assertFalse(useCase("newUser2@lacuc.com", "password"))
+        Assert.assertTrue(useCase("oldUser1@lacuc.com", "password"))
+        Mockito.verify(loginService, Mockito.times(3)).signIn(Mockito.anyMap())
     }
 
 }
