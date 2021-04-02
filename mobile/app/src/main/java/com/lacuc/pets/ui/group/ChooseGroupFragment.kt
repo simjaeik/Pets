710266cb552d3edb612012fc673918b501db4f2d
SeongHwan Kim <shshksh58@gmail.com>
2021-04-02 11:56:07 +0900

    [feat] 그룹 화면 설정
    - ChooseGroupFragment에 데이터 바인딩을 적용하고 리사이클러 뷰의 어댑터와
    레이아웃 매니저를 설정.

--- mobile/app/src/main/java/com/lacuc/pets/ui/group/ChooseGroupFragment.kt
+++ mobile/app/src/main/java/com/lacuc/pets/ui/group/ChooseGroupFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.lacuc.pets.ViewModelFactory
 import com.lacuc.pets.databinding.FragmentChooseGroupBinding
 
@@ -15,6 +16,8 @@ class ChooseGroupFragment : Fragment() {
 
     private val viewModel: ChooseGroupViewModel by viewModels { ViewModelFactory() }
 
+    lateinit var adapter: ChooseGroupAdapter
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -22,6 +25,21 @@ class ChooseGroupFragment : Fragment() {
     ): View {
         _binding = FragmentChooseGroupBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.lifecycleOwner = viewLifecycleOwner
+        binding.vm = viewModel
+
+        adapter = ChooseGroupAdapter()
+
+        binding.recyclerViewChooseGroup.apply {
+            layoutManager = LinearLayoutManager(context)
+            adapter = this@ChooseGroupFragment.adapter
+        }
+
     }
 
     override fun onDestroy() {
