5319e3af5a3e9693070112a7da4bbecb1aee7cc1
SeongHwan Kim <shshksh58@gmail.com>
2021-04-02 11:53:35 +0900

    [feat] ChooseGroupViewModel 인스턴스를 팩토리에 추가
    - 뷰 모델 팩토리에 가짜 그룹 리포지토리를 파라미터로 받는
    ChooseGroupViewModel 인스턴스를 추가함.

--- mobile/app/src/main/java/com/lacuc/pets/ViewModelFactory.kt
+++ mobile/app/src/main/java/com/lacuc/pets/ViewModelFactory.kt
@@ -2,8 +2,11 @@ package com.lacuc.pets
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import com.lacuc.pets.data.FakeGroupRepository
 import com.lacuc.pets.data.LoginService
+import com.lacuc.pets.domain.group.GetGroupUseCase
 import com.lacuc.pets.domain.login.SignUpUseCase
+import com.lacuc.pets.ui.group.ChooseGroupViewModel
 import com.lacuc.pets.ui.login.signup.SignUpViewModel
 
 class ViewModelFactory : ViewModelProvider.Factory {
@@ -15,6 +18,10 @@ class ViewModelFactory : ViewModelProvider.Factory {
                 isAssignableFrom(SignUpViewModel::class.java) ->
                     SignUpViewModel(
                         SignUpUseCase(object : LoginService {})
+                    )
+                isAssignableFrom(ChooseGroupViewModel::class.java) ->
+                    ChooseGroupViewModel(
+                        GetGroupUseCase(FakeGroupRepository())
                     )
                 else ->
                     throw IllegalArgumentException("Unknown ViewModel class: ${modelClass.name}")
