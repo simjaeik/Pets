25ab5ecd253669de27bd8a29f157af14d9e526dc
SeongHwan Kim <shshksh58@gmail.com>
2021-04-03 16:16:28 +0900

    [fix] 그룹 아이템 클릭 리스너 설정
    - 그룹 아이템 클릭 시 아이템에 등록된 리스너 함수를 호출하도록 설정.

--- mobile/app/src/main/java/com/lacuc/pets/ui/group/ChooseGroupFragment.kt
+++ mobile/app/src/main/java/com/lacuc/pets/ui/group/ChooseGroupFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.lacuc.pets.ViewModelFactory
@@ -30,6 +31,10 @@ class ChooseGroupFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         binding.lifecycleOwner = viewLifecycleOwner
+
+        viewModel.clickItem.observe(viewLifecycleOwner) {
+            Toast.makeText(context, "clicked: $it", Toast.LENGTH_SHORT).show()
+        }
     }
 
     override fun onDestroy() {
