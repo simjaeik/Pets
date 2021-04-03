f00baf76f6e6be08cd7424666cd7c27a465995a5
SeongHwan Kim <shshksh58@gmail.com>
2021-04-03 15:50:43 +0900

    [fix] 그룹 리스트 레이아웃 문제 수정
    - 리사이클러 뷰의 하단이 잘리는 문제와 아이템 너비의 match_parent 속성이
    동작하지 않는 이슈가 발생함. 리사이클러 뷰의 제약 조건을 명시하고 그룹
    리스트가 바인딩 될 때 generateDefaultLayoutParams()를 오버라이드한 레이아웃
    매니저를 지정하는 것으로 해결함.

--- mobile/app/src/main/java/com/lacuc/pets/ui/group/GroupListBinding.kt
+++ mobile/app/src/main/java/com/lacuc/pets/ui/group/GroupListBinding.kt
@@ -1,6 +1,8 @@
 package com.lacuc.pets.ui.group
 
+import android.view.ViewGroup
 import androidx.databinding.BindingAdapter
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.lacuc.pets.domain.group.GroupItem
 
@@ -8,5 +10,12 @@ import com.lacuc.pets.domain.group.GroupItem
 fun setItems(listView: RecyclerView, items: List<GroupItem>) {
     if (listView.adapter == null)
         listView.adapter = ChooseGroupAdapter()
+    listView.layoutManager = object : LinearLayoutManager(listView.context) {
+        override fun generateDefaultLayoutParams(): RecyclerView.LayoutParams =
+            RecyclerView.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT
+            )
+    }
     (listView.adapter as ChooseGroupAdapter).addList(items)
 }
