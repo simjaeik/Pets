67324d685c7b20e69a13563103e29c401ad01f69
SeongHwan Kim <shshksh58@gmail.com>
2021-04-03 17:45:10 +0900

    [feat] ChooseGroupFragment 툴바 옵션 메뉴 설정
    - 그룹 생성 화면으로 이동할 옵션 메뉴 리소스를 생성하고 프래그먼트에서 메뉴를
    인플레이트하도록 설정.

--- mobile/app/src/main/java/com/lacuc/pets/ui/group/ChooseGroupFragment.kt
+++ mobile/app/src/main/java/com/lacuc/pets/ui/group/ChooseGroupFragment.kt
@@ -7,14 +7,22 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.navigation.NavController
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
+import com.lacuc.pets.R
 import com.lacuc.pets.ViewModelFactory
 import com.lacuc.pets.databinding.FragmentChooseGroupBinding
+import timber.log.Timber
 
 class ChooseGroupFragment : Fragment() {
     private var _binding: FragmentChooseGroupBinding? = null
     private val binding get() = _binding!!
 
     private val viewModel: ChooseGroupViewModel by viewModels { ViewModelFactory() }
+
+    private val navController: NavController by lazy { findNavController() }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -32,8 +40,20 @@ class ChooseGroupFragment : Fragment() {
 
         binding.lifecycleOwner = viewLifecycleOwner
 
+        setupToolbar()
+
         viewModel.clickItem.observe(viewLifecycleOwner) {
             Toast.makeText(context, "clicked: $it", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun setupToolbar() {
+        val appBarConfiguration = AppBarConfiguration(setOf(R.id.chooseGroupFragment))
+        binding.toolbarChooseGroup.setupWithNavController(navController, appBarConfiguration)
+        binding.toolbarChooseGroup.inflateMenu(R.menu.menu_group)
+        binding.toolbarChooseGroup.setOnMenuItemClickListener {
+            Timber.d("onOptionsItemSelected")
+            true
         }
     }
 
