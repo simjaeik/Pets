7e98f4e3c08221253c9c7f7f0c8a1c2985e97361
SeongHwan Kim <shshksh58@gmail.com>
2021-04-03 15:47:10 +0900

    [feat] 그룹 아이템에 데이터 바인딩 적용
    - 그룹 아이템에서 각 요소의 값을 바인딩 변수의 값으로 설정하고 이미지를 얻기
    위해 Glide를 사용하는 바인딩 어댑터 추가.

--- mobile/app/src/main/java/com/lacuc/pets/domain/group/GroupItem.kt
+++ mobile/app/src/main/java/com/lacuc/pets/domain/group/GroupItem.kt
@@ -3,4 +3,10 @@ package com.lacuc.pets.domain.group
 import com.lacuc.pets.data.Group
 
 class GroupItem(val group: Group, val clickListener: (GroupItem) -> Unit) {
+
+    fun getName() = group.name
+
+    fun getInfo() = group.info
+
+    fun getImage() = group.image
 }
