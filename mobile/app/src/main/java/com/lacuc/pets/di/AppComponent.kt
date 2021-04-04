b0151b202510336d3c12c1038cc8647d392e92f6
SeongHwan Kim <shshksh58@gmail.com>
2021-04-04 15:36:38 +0900

    [feat] 애플리케이션 수준 의존성 설정
    - 애플리케이션과 애플리케이션 컨텍스트를 제공하는 모듈을 만들고
    AppComponent에 연결. App이 DaggerApplication을 상속하도록 변경하고
    applicationInject()를 오버라이드함.

---
+++ mobile/app/src/main/java/com/lacuc/pets/di/AppComponent.kt
@@ -0,0 +1,21 @@
+package com.lacuc.pets.di
+
+import com.lacuc.pets.App
+import dagger.Component
+import dagger.android.AndroidInjector
+import dagger.android.support.AndroidSupportInjectionModule
+import javax.inject.Singleton
+
+@Singleton
+@Component(
+    modules = [
+        AndroidSupportInjectionModule::class,
+        AppModule::class
+    ]
+)
+interface AppComponent : AndroidInjector<App> {
+
+    @Component.Factory
+    abstract class Factory : AndroidInjector.Factory<App>
+
+}
