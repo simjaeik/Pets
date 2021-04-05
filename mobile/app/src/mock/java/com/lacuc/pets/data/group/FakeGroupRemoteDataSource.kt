eaa6d2e0503501636d017e1d4927b1108b4f4055
SeongHwan Kim <shshksh58@gmail.com>
2021-04-05 15:14:24 +0900

    [feat] FakeGroupRemoteDataSource 자료구조 변경
    - 유저가 속한 그룹을 이메일로 찾을 수 있도록 리스트에서 맵으로 변경함.

--- mobile/app/src/mock/java/com/lacuc/pets/data/group/FakeGroupRemoteDataSource.kt
+++ mobile/app/src/mock/java/com/lacuc/pets/data/group/FakeGroupRemoteDataSource.kt
@@ -4,26 +4,33 @@ import javax.inject.Inject
 
 class FakeGroupRemoteDataSource @Inject constructor() : GroupDataSource {
 
-    private val groupData = mutableListOf(
-        Group(
-            "Group1",
-            "그룹 1 소개",
-            "https://images.unsplash.com/photo-1593643946890-b5b85ade6451?ixid=MXwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=2129&q=80",
-            false
-        ),
-        Group(
-            "Group2",
-            "aco wqp awji  snl cnqwo c aslc wiz xlc  wia cwalwcia sic raltj wj cor fosajrw sa w skaj",
-            "https://images.unsplash.com/photo-1617289749213-c2a7b44f6523?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1914&q=80",
-            false
-        ),
-        Group(
-            "Group3",
-            "info \n info \n info",
-            "https://images.unsplash.com/photo-1617286856618-f9411fcc1800?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80",
-            false
-        ),
-    )
+    private val groupData =
+        mutableMapOf<String, MutableList<Group>>().withDefault { mutableListOf() }
 
-    override fun loadGroup(email: String): List<Group> = groupData
+    init {
+        groupData["tempUser@lacuc.com"]?.addAll(
+            listOf(
+                Group(
+                    "Group1",
+                    "그룹 1 소개",
+                    "https://images.unsplash.com/photo-1593643946890-b5b85ade6451?ixid=MXwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=2129&q=80",
+                    false
+                ),
+                Group(
+                    "Group2",
+                    "aco wqp awji  snl cnqwo c aslc wiz xlc  wia cwalwcia sic raltj wj cor fosajrw sa w skaj",
+                    "https://images.unsplash.com/photo-1617289749213-c2a7b44f6523?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1914&q=80",
+                    false
+                ),
+                Group(
+                    "Group3",
+                    "info \n info \n info",
+                    "https://images.unsplash.com/photo-1617286856618-f9411fcc1800?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80",
+                    false
+                )
+            )
+        )
+    }
+
+    override fun loadGroup(email: String): List<Group> = groupData[email] ?: emptyList()
 }
