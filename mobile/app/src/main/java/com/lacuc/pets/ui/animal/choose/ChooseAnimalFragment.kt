3c54c4b0faec45ecd5295b3fab360f108119d1a4
SeongHwan Kim <shshksh58@gmail.com>
2021-04-07 14:19:53 +0900

    [feat] ChooseAnimalViewModel 구현
    - 유스케이스에 리스너를 넘겨 얻은 AnimalItem 리스트로 라이브 데이터를
    업데이트하고 프래그먼트에서 클릭한 아이템을 관찰하며 토스트를 출력하도록
    구현함.

--- mobile/app/src/main/java/com/lacuc/pets/ui/animal/choose/ChooseAnimalFragment.kt
+++ mobile/app/src/main/java/com/lacuc/pets/ui/animal/choose/ChooseAnimalFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.NavController
@@ -42,6 +43,12 @@ class ChooseAnimalFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         setupToolbar()
+
+        viewModel.loadGroups()
+
+        viewModel.clickItem.observe(viewLifecycleOwner) {
+            Toast.makeText(context, "click: $it", Toast.LENGTH_SHORT).show()
+        }
     }
 
     private fun setupToolbar() {
--- mobile/app/src/main/java/com/lacuc/pets/ui/animal/choose/ChooseAnimalViewModel.kt
+++ mobile/app/src/main/java/com/lacuc/pets/ui/animal/choose/ChooseAnimalViewModel.kt
@@ -1,6 +1,8 @@
 package com.lacuc.pets.ui.animal.choose
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.lacuc.pets.domain.animal.AnimalItem
 import com.lacuc.pets.domain.animal.GetAnimalUseCase
 import javax.inject.Inject
 
@@ -8,4 +10,13 @@ class ChooseAnimalViewModel @Inject constructor(
     val getAnimalUseCase: GetAnimalUseCase
 ) : ViewModel() {
 
+    val animalItems = MutableLiveData<List<AnimalItem>>()
+
+    val clickItem = MutableLiveData<AnimalItem>()
+
+    fun loadGroups() {
+        animalItems.value = getAnimalUseCase(1) {
+            clickItem.value = it
+        }
+    }
 }
