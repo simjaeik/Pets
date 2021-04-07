88570152bcccdba0696cfd67ed45030ff43a4e56
SeongHwan Kim <shshksh58@gmail.com>
2021-04-07 15:56:35 +0900

    [feat] BindableAdapter 적용
    - 바인딩 어댑터에서 여러 타입의 데이터를 받을 수 있도록 바인딩 어댑터와
    리사이클러 뷰 어댑터의 구조를 변경함.

---
+++ mobile/app/src/main/java/com/lacuc/pets/util/BindableAdapter.kt
@@ -0,0 +1,5 @@
+package com.lacuc.pets.util
+
+interface BindableAdapter<T> {
+    fun setData(data: T)
+}
--- mobile/app/src/main/java/com/lacuc/pets/util/BindingAdpaters.kt
+++ mobile/app/src/main/java/com/lacuc/pets/util/BindingAdpaters.kt
@@ -1,31 +1,21 @@
 package com.lacuc.pets.util
 
-import android.view.ViewGroup
 import android.widget.ImageView
 import androidx.databinding.BindingAdapter
-import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
-import com.lacuc.pets.domain.group.GroupItem
-import com.lacuc.pets.ui.group.choose.ChooseGroupAdapter
 
-@BindingAdapter("app:image")
+@BindingAdapter("image")
 fun setImage(imageView: ImageView, url: String) {
     Glide.with(imageView.context)
         .load(url)
         .into(imageView)
 }
 
-@BindingAdapter("app:items")
-fun setItems(listView: RecyclerView, items: List<GroupItem>) {
-    if (listView.adapter == null)
-        listView.adapter = ChooseGroupAdapter()
-    listView.layoutManager = object : LinearLayoutManager(listView.context) {
-        override fun generateDefaultLayoutParams(): RecyclerView.LayoutParams =
-            RecyclerView.LayoutParams(
-                ViewGroup.LayoutParams.MATCH_PARENT,
-                ViewGroup.LayoutParams.WRAP_CONTENT
-            )
+@BindingAdapter("items")
+@Suppress("UNCHECKED_CAST")
+fun <T> setItems(listView: RecyclerView, items: T) {
+    if (listView.adapter is BindableAdapter<*>) {
+        (listView.adapter as BindableAdapter<T>).setData(items)
     }
-    (listView.adapter as ChooseGroupAdapter).addList(items)
 }
