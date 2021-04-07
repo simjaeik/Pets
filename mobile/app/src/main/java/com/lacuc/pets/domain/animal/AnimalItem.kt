81aece5bb869378fbdd565c2571eae15e4565fe2
SeongHwan Kim <shshksh58@gmail.com>
2021-04-07 15:59:42 +0900

    [feat] AnimalItem에 데이터 바인딩 적용
    - AnimalItem이 바인딩 변수로 값을 설정할 수 있도록 구현함.

--- mobile/app/src/main/java/com/lacuc/pets/domain/animal/AnimalItem.kt
+++ mobile/app/src/main/java/com/lacuc/pets/domain/animal/AnimalItem.kt
@@ -4,4 +4,16 @@ import com.lacuc.pets.data.animal.Animal
 
 class AnimalItem(val animal: Animal, val clickListener: (AnimalItem) -> Unit) {
 
+    fun getName() = animal.name
+
+    fun getSubspecies() = animal.subspecies
+
+    fun getImage() = animal.image
+
+    fun getAge() = "${animal.age}세"
+
+    fun getSex() = animal.sex
+
+    fun getWeight() = animal.weight.toString()
+
 }
