728133b6b4031a51847cc29d0de683e38f958969
SeongHwan Kim <shshksh58@gmail.com>
2021-04-08 15:32:08 +0900

    [feat] AddAnimal 프래그먼트 설정
    - 툴바 바인딩, 이미지 버튼으로 이미지를 고를 수 있도록 클릭 리스너 추가.

--- mobile/app/src/main/java/com/lacuc/pets/ui/animal/add/AddAnimalFragment.kt
+++ mobile/app/src/main/java/com/lacuc/pets/ui/animal/add/AddAnimalFragment.kt
@@ -1,12 +1,17 @@
 package com.lacuc.pets.ui.animal.add
 
+import android.content.Intent
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.viewModels
 import androidx.navigation.NavController
 import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
 import com.lacuc.pets.ViewModelFactory
 import com.lacuc.pets.databinding.FragmentAddAnimalBinding
 import dagger.android.support.DaggerFragment
@@ -23,6 +28,11 @@ class AddAnimalFragment : DaggerFragment() {
 
     private val navController: NavController by lazy { findNavController() }
 
+    private val requestActivity =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+
+        }
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -33,6 +43,22 @@ class AddAnimalFragment : DaggerFragment() {
             vm = viewModel
         }
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setupToolbar()
+
+        binding.btnAddAnimalPickImage.setOnClickListener {
+            val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI)
+            requestActivity.launch(intent)
+        }
+    }
+
+    private fun setupToolbar() {
+        val appBarConfiguration = AppBarConfiguration(navController.graph)
+        binding.toolbarAddAnimal.setupWithNavController(navController, appBarConfiguration)
     }
 
     override fun onDestroyView() {
