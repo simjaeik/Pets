cadbf297cf3b5428c2359aab2eb520ab671f3ea8
SeongHwan Kim <shshksh58@gmail.com>
2021-04-09 11:41:33 +0900

    [feat] AnimalDetail 관련 요소 초기 설정
    - 프래그먼트와 레이아웃, 뷰모델을 생성하고 데이터바인딩을 적용.

---
+++ mobile/app/src/main/java/com/lacuc/pets/ui/animal/detail/AnimalDetailFragment.kt
@@ -0,0 +1,38 @@
+package com.lacuc.pets.ui.animal.detail
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import com.lacuc.pets.ViewModelFactory
+import com.lacuc.pets.databinding.FragmentAnimalDetailBinding
+
+class AnimalDetailFragment : Fragment() {
+
+    private var _binding: FragmentAnimalDetailBinding? = null
+    private val binding get() = _binding!!
+
+    lateinit var viewModelFactory: ViewModelFactory
+
+    private val viewModel: AnimalDetailViewModel by viewModels { viewModelFactory }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentAnimalDetailBinding.inflate(inflater, container, false).apply {
+            lifecycleOwner = viewLifecycleOwner
+            vm = viewModel
+        }
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+}
---
+++ mobile/app/src/main/java/com/lacuc/pets/ui/animal/detail/AnimalDetailViewModel.kt
@@ -0,0 +1,7 @@
+package com.lacuc.pets.ui.animal.detail
+
+import androidx.lifecycle.ViewModel
+
+class AnimalDetailViewModel: ViewModel() {
+
+}
