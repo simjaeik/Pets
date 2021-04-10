55951490dc8b67abd573428ad9af9c9fdbde0b59
SeongHwan Kim <shshksh58@gmail.com>
2021-04-10 20:59:10 +0900

    [feat] 뷰 모델 초기화 함수 구현
    - 처음 화면이 나타났을 때 정보 탭을 보여주기 위해 아이템 리스트를 초기화하는
    함수를 구현. 생성자에서 Animal 데이터를 얻는 유스케이스를 추가할 수도 있지만
    이미 이전에 조회한 데이터를 사용하기 위해 함수로 분리함.

--- mobile/app/src/main/java/com/lacuc/pets/ui/animal/detail/AnimalDetailViewModel.kt
+++ mobile/app/src/main/java/com/lacuc/pets/ui/animal/detail/AnimalDetailViewModel.kt
@@ -1,8 +1,16 @@
 package com.lacuc.pets.ui.animal.detail
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import javax.inject.Inject
 
-class AnimalDetailViewModel @Inject constructor(): ViewModel() {
+class AnimalDetailViewModel @Inject constructor() : ViewModel() {
 
+    val detailItems = MutableLiveData<List<AnimalDetailItem>>()
+
+    fun initItem(item: AnimalDetailItem) {
+        if (detailItems.value.isNullOrEmpty()) {
+            detailItems.value = listOf(item)
+        }
+    }
 }
