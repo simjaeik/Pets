a241828bb3db48e2421e21a27b1ff7c6c3d09b9b
SeongHwan Kim <shshksh58@gmail.com>
2021-04-10 21:00:43 +0900

    [feat] 리사이클러 뷰 설정
    - AnimalDetailFragment에서 리사이클러 뷰의 어댑터와 레이아웃 매니저를
    설정하고, 아이템을 초기화함.

--- mobile/app/src/main/java/com/lacuc/pets/ui/animal/detail/AnimalDetailFragment.kt
+++ mobile/app/src/main/java/com/lacuc/pets/ui/animal/detail/AnimalDetailFragment.kt
@@ -6,6 +6,8 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.navArgs
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.lacuc.pets.ViewModelFactory
 import com.lacuc.pets.databinding.FragmentAnimalDetailBinding
 import dagger.android.support.DaggerFragment
@@ -34,6 +36,23 @@ class AnimalDetailFragment : DaggerFragment() {
             item = args.animal
         }
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.recyclerviewAnimalDetail.apply {
+            adapter = AnimalDetailAdapter()
+            layoutManager = object : LinearLayoutManager(context) {
+                override fun generateDefaultLayoutParams(): RecyclerView.LayoutParams =
+                    RecyclerView.LayoutParams(
+                        ViewGroup.LayoutParams.MATCH_PARENT,
+                        ViewGroup.LayoutParams.WRAP_CONTENT
+                    )
+            }
+        }
+
+        viewModel.initItem(AnimalDetailDetailItem(args.animal))
     }
 
     override fun onDestroyView() {
