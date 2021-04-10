eccc75c0e7977de1e570f88e7f76e7111194eeb6
SeongHwan Kim <shshksh58@gmail.com>
2021-04-10 22:45:13 +0900

    [feat] 상세 화면 아이템 스위칭 구현
    - 뷰 모델이 탭의 position을 파라미터로 받아, 선택한 탭에 맞게 적절한
    아이템을 설정하는 함수를 추가함. private 프로퍼티는 이전에 조회한 값을 위한
    캐시 용도로 사용하며 외부에는 detailItems만 노출한다.

--- mobile/app/src/main/java/com/lacuc/pets/ui/animal/detail/AnimalDetailViewModel.kt
+++ mobile/app/src/main/java/com/lacuc/pets/ui/animal/detail/AnimalDetailViewModel.kt
@@ -2,15 +2,33 @@ package com.lacuc.pets.ui.animal.detail
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.lacuc.pets.domain.animal.GetMedicalUseCase
+import com.lacuc.pets.domain.animal.GetMemoUseCase
 import javax.inject.Inject
 
-class AnimalDetailViewModel @Inject constructor() : ViewModel() {
+class AnimalDetailViewModel @Inject constructor(
+    private val getMedicalUseCase: GetMedicalUseCase,
+    private val getMemoUseCase: GetMemoUseCase,
+) : ViewModel() {
 
     val detailItems = MutableLiveData<List<AnimalDetailItem>>()
 
-    fun initItem(item: AnimalDetailItem) {
+    private lateinit var detailItem: List<AnimalDetailDetailItem>
+    private val medicalItem: List<AnimalDetailMedicalItem> by lazy { getMedicalUseCase(1) }
+    private val memoItem: List<AnimalDetailMemoItem> by lazy { getMemoUseCase(1) }
+
+    fun initItem(item: AnimalDetailDetailItem) {
         if (detailItems.value.isNullOrEmpty()) {
-            detailItems.value = listOf(item)
+            detailItem = listOf(item)
+            detailItems.value = detailItem
+        }
+    }
+
+    fun switchItem(position: Int) {
+        when (position) {
+            0 -> detailItems.value = detailItem
+            1 -> detailItems.value = medicalItem
+            2 -> detailItems.value = memoItem
         }
     }
 }
