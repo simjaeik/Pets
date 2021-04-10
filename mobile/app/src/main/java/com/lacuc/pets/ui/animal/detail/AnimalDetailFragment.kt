7cb6a443d58bb0f352764e63368e71229b30814e
SeongHwan Kim <shshksh58@gmail.com>
2021-04-10 17:20:32 +0900

    [feat] Animal 파라미터 바인딩
    - AnimalDetailFragment에서 파라미터로 받은 Animal 인스턴스를 바인딩하고,
    툴바에서 동물 이미지와 이름을 사용함.

--- mobile/app/src/main/java/com/lacuc/pets/ui/animal/detail/AnimalDetailFragment.kt
+++ mobile/app/src/main/java/com/lacuc/pets/ui/animal/detail/AnimalDetailFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.navArgs
 import com.lacuc.pets.ViewModelFactory
 import com.lacuc.pets.databinding.FragmentAnimalDetailBinding
 import dagger.android.support.DaggerFragment
@@ -20,6 +21,8 @@ class AnimalDetailFragment : DaggerFragment() {
 
     private val viewModel: AnimalDetailViewModel by viewModels { viewModelFactory }
 
+    private val args: AnimalDetailFragmentArgs by navArgs()
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -28,6 +31,7 @@ class AnimalDetailFragment : DaggerFragment() {
         _binding = FragmentAnimalDetailBinding.inflate(inflater, container, false).apply {
             lifecycleOwner = viewLifecycleOwner
             vm = viewModel
+            item = args.animal
         }
         return binding.root
     }
