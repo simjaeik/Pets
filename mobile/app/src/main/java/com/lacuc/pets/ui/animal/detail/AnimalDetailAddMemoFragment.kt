9b919677c9ab2929bcc68e558b82ddfdaba7c063
SeongHwan Kim <shshksh58@gmail.com>
2021-04-13 14:34:52 +0900

    [feat] 메모 추가 레이아웃, 뷰 모델 생성

---
+++ mobile/app/src/main/java/com/lacuc/pets/ui/animal/detail/AnimalDetailAddMemoFragment.kt
@@ -0,0 +1,44 @@
+package com.lacuc.pets.ui.animal.detail
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.navigation.NavController
+import androidx.navigation.fragment.findNavController
+import com.lacuc.pets.ViewModelFactory
+import com.lacuc.pets.databinding.FragmentAddMemoBinding
+import javax.inject.Inject
+
+class AnimalDetailAddMemoFragment : Fragment() {
+
+    private var _binding: FragmentAddMemoBinding? = null
+    private val binding get() = _binding!!
+
+    @Inject
+    lateinit var viewModelFactory: ViewModelFactory
+
+    private val viewModel: AnimalDetailAddMemoViewModel by viewModels { viewModelFactory }
+
+    private val navController: NavController by lazy { findNavController() }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentAddMemoBinding.inflate(inflater, container, false).apply {
+            lifecycleOwner = viewLifecycleOwner
+            vm = viewModel
+        }
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+}
---
+++ mobile/app/src/main/java/com/lacuc/pets/ui/animal/detail/AnimalDetailAddMemoViewModel.kt
@@ -0,0 +1,13 @@
+package com.lacuc.pets.ui.animal.detail
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class AnimalDetailAddMemoViewModel : ViewModel() {
+
+    val content = MutableLiveData("")
+
+    fun onCompleteClick() {
+
+    }
+}
