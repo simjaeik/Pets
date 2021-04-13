978175a8f723b55a753ddb7a18c0d62c5dfbd44c
SeongHwan Kim <shshksh58@gmail.com>
2021-04-13 10:54:55 +0900

    [feat] 병원 기록 프래그먼트, 뷰 모델 추가
    - 프래그먼트와 뷰 모델 초기 설정.

---
+++ mobile/app/src/main/java/com/lacuc/pets/ui/animal/detail/AnimalDetailAddMedicalFragment.kt
@@ -0,0 +1,40 @@
+package com.lacuc.pets.ui.animal.detail
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import com.lacuc.pets.ViewModelFactory
+import com.lacuc.pets.databinding.FragmentAddMedicalBinding
+import dagger.android.support.DaggerFragment
+
+class AnimalDetailAddMedicalFragment : DaggerFragment() {
+    private var _binding: FragmentAddMedicalBinding? = null
+    private val binding get() = _binding!!
+
+    lateinit var viewModelFactory: ViewModelFactory
+
+    private val viewModel: AnimalDetailAddMedicalViewModel by viewModels { viewModelFactory }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentAddMedicalBinding.inflate(inflater, container, false).apply {
+            lifecycleOwner = viewLifecycleOwner
+        }
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+}
---
+++ mobile/app/src/main/java/com/lacuc/pets/ui/animal/detail/AnimalDetailAddMedicalViewModel.kt
@@ -0,0 +1,6 @@
+package com.lacuc.pets.ui.animal.detail
+
+import androidx.lifecycle.ViewModel
+
+class AnimalDetailAddMedicalViewModel: ViewModel() {
+}
