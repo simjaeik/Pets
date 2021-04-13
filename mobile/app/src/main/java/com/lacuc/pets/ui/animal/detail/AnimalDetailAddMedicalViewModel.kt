2fb6072aa1e729bcc6764d10dab813632a229040
SeongHwan Kim <shshksh58@gmail.com>
2021-04-13 11:01:36 +0900

    [feat] 뷰 모델 바인딩
    - 병원 기록 추가 레이아웃과 뷰 모델에 데이터 바인딩 적용.

--- mobile/app/src/main/java/com/lacuc/pets/ui/animal/detail/AnimalDetailAddMedicalViewModel.kt
+++ mobile/app/src/main/java/com/lacuc/pets/ui/animal/detail/AnimalDetailAddMedicalViewModel.kt
@@ -1,6 +1,12 @@
 package com.lacuc.pets.ui.animal.detail
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class AnimalDetailAddMedicalViewModel: ViewModel() {
+class AnimalDetailAddMedicalViewModel : ViewModel() {
+
+    val title = MutableLiveData<String>()
+    val hospital = MutableLiveData<String>()
+    val time = MutableLiveData<String>()
+    val content = MutableLiveData<String>()
 }
