df8a7dc7c076a067e90fdb2140697528f32b1354
hyerimseong <shl7810@naver.com>
2021-04-14 23:58:04 +0900

    [fix] post 라디오 버튼 조건 수정

--- web/client/post/post.js
+++ web/client/post/post.js
@@ -1,11 +1,22 @@
 //제목, 닉네임, 내용이 비어있으면 화면이동이 안되게 한다
 function check_blank(){
 
+    let radiobtn = document.getElementsByName("chk_info");
     let checktitle = document.getElementById("posttitle");
     let checkname = document.getElementById("writername");
     let checkcontent = document.getElementById("content");
 
-    if ( checktitle.value === "") { alert(" 제목이 입력되지 않았습니다. "); }
+    let cnt = 0, check = false;
+    for (let i=0;i<radiobtn.length;i++){
+        
+        if(radiobtn[i].checked === false) { cnt++; }
+        
+        if ( cnt === radiobtn.length) { check = false; }
+        else { check = true; } 
+    }
+
+    if ( check === false) { alert("게시물을 선택해주세요. "); }
+    else if ( checktitle.value === "") { alert(" 제목이 입력되지 않았습니다. "); }
     else if ( checkname.value === "") { alert(" 닉네임이 입력되지 않았습니다. "); }
     else if ( checkcontent.value === "") { alert(" 내용이 입력되지 않았습니다. ")}
     else{
