11cfb1d855b86a75dcffc41dd5d5461b51a3211e
SeongHwan Kim <shshksh58@gmail.com>
2021-04-18 21:52:18 +0900

    [feat] AniamlService 작성
    - 그룹 서비스가 제공할 함수를 API 명세서 형식에 맞게 추가함.

---
+++ mobile/app/src/main/java/com/lacuc/pets/data/AnimalService.kt
@@ -0,0 +1,34 @@
+package com.lacuc.pets.data
+
+import com.lacuc.pets.data.animal.Animal
+import com.lacuc.pets.data.animal.Memo
+import retrofit2.http.*
+
+interface AnimalService {
+    @GET("api/animal/all/{id}")
+    fun getAnimalByGroup(@Path("id") id: Int)
+
+    @POST("api/animal")
+    fun addAnimal(@Body animal: Animal)
+
+    @DELETE("api/animal/{id}")
+    fun deleteAnimal(@Path("id") id: Int)
+
+    @GET("api/animal/{id}")
+    fun getAnimal(@Path("id") id: Int)
+
+    @PATCH("api/animal/{id}")
+    fun updateAnimalDetail(@Path("id") id: Int, @Body animal: Animal)
+
+    @GET("api/animal/memo/{id}")
+    fun getMemos(@Path("id") id: Int)
+
+    @POST("api/animal/memo/{id}")
+    fun setMemo(@Path("id") id: Int, @Body memo: Memo)
+
+    @PATCH("api/animal/memo/{id}")
+    fun updateMemo(@Path("id") id: Int, @Body memo: Memo)
+
+    @DELETE("api/animal/memo/{id}")
+    fun deleteMemo(@Path("id") id: Int)
+}
