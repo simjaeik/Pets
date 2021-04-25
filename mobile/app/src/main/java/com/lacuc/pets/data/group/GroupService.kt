8206ca1d48b95853a49e7a17c8860eaae8b8781f
SeongHwan Kim <shshksh58@gmail.com>
2021-04-25 22:19:46 +0900

    [refactor] 그룹 서비스 함수 시그니처 변경
    - 함수 호출에 따라 알맞게 반환값을 리턴하도록 리턴 타입을 각각 설정하고,
    suspend 함수로 변경.

--- mobile/app/src/main/java/com/lacuc/pets/data/group/GroupService.kt
+++ mobile/app/src/main/java/com/lacuc/pets/data/group/GroupService.kt
@@ -1,51 +1,55 @@
 package com.lacuc.pets.data.group
 
+import com.lacuc.pets.data.Result
 import com.lacuc.pets.data.group.entity.Group
 import retrofit2.http.*
 
 interface GroupService {
     @GET("api/group")
-    fun getMyGroups()
+    suspend fun getMyGroups(): Result<List<Group>>
 
     @GET("api/group/near")
-    fun getGroupsNear(@Query("latitude") latitude: Double, @Query("longitude") longitude: Double)
+    suspend fun getGroupsNear(
+        @Query("latitude") latitude: Double,
+        @Query("longitude") longitude: Double
+    ): Result<List<Group>>
 
     @DELETE("api/group/{id}")
-    fun deleteGroup(@Path("id") id: Int) // 그룹의 GID?
+    suspend fun deleteGroup(@Path("id") gid: Int): Result<Void>
 
     @POST("api/group")
-    fun setGroup(@Body group: Group)
+    suspend fun setGroup(@Body group: Group): Result<Void>
 
     @GET("api/group/{id}")
-    fun getGroup(@Path("id") id: Int)
+    suspend fun getGroup(@Path("id") gid: Int): Result<Group>
 
     @FormUrlEncoded
     @POST("api/group/member")
-    fun addGroupMember(@FieldMap memberParams: Map<String, Any>)
+    suspend fun addGroupMember(@FieldMap memberParams: Map<String, Any>): Result<Void>
 
     @GET("api/group/member/{id}")
-    fun getGroupMembers(@Path("id") id: Int)
+    suspend fun getGroupMembers(@Path("id") gid: Int): Result<List<Unit>> // TODO: 2021-04-25 리턴 타입: Member
 
     @DELETE("api/group/members/{id}")
-    fun deleteGroupMember(@Path("id") id: Int)
+    suspend fun deleteGroupMember(@Path("id") gid: Int): Result<Void>
 
     @GET("api/group/images/{id}")
-    fun getGroupImages(@Path("id") id: Int)
+    suspend fun getGroupImages(@Path("id") gid: Int): Result<List<Unit>> // TODO: 2021-04-25 리턴 타입: GroupImage
     // 그룹 단위 갤러리의 이미지를 가져오는 함수
 
     @FormUrlEncoded
     @POST("api/group/images")
-    fun setGroupImage(@FieldMap imageParams: Map<String, Any>)
+    suspend fun setGroupImage(@FieldMap imageParams: Map<String, Any>): Result<List<Unit>>
 
     @GET("api/item/{id}")
-    fun getItems(@Path("id") id: Int)
+    suspend fun getItems(@Path("id") gid: Int): Result<List<Unit>> // TODO: 2021-04-25 리턴 타입: Item
 
     @POST("api/item")
-    fun setItem(@Body item: Any) // TODO: 2021-04-18 파라미터 타입을 Item으로 변경해야 함
+    suspend fun setItem(@Body item: Any): Result<Void> // TODO: 2021-04-18 파라미터 타입을 Item으로 변경해야 함
 
     @PATCH("api/item/{id}")
-    fun updateItem(@Path("id") id: Int, @Body item: Any)
+    suspend fun updateItem(@Path("id") iid: Int, @Body item: Any): Result<Void>
 
     @DELETE("api/item/{id}")
-    fun deleteItem(@Path("id") id: Int)
+    suspend fun deleteItem(@Path("id") iid: Int): Result<Void>
 }
