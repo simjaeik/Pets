24db0b6b8a85b1d0ffaa0a740020659df479166d
SeongHwan Kim <shshksh58@gmail.com>
2021-04-26 17:35:36 +0900

    [feat] animal api 시그니처 수정
    - api 함수명과 리턴 타입, 매개변수를 명세서에 맞게 수정함.

--- mobile/app/src/main/java/com/lacuc/pets/data/animal/AnimalRepository.kt
+++ mobile/app/src/main/java/com/lacuc/pets/data/animal/AnimalRepository.kt
@@ -1,20 +1,32 @@
 package com.lacuc.pets.data.animal
 
+import com.lacuc.pets.data.Result
 import com.lacuc.pets.data.animal.entity.Animal
 import com.lacuc.pets.data.animal.entity.Medical
 import com.lacuc.pets.data.animal.entity.Memo
 
 interface AnimalRepository {
-    fun loadAnimal(gid: Int): List<Animal>
 
-    fun addAnimal(animal: Animal)
+    suspend fun getAnimalByGroup(gid: Int): Result<List<Animal>>
 
-    fun loadMedical(aid: Int): List<Medical>
+    suspend fun addAnimal(animal: Animal): Result<Void>
 
-    fun loadMemo(aid: Int): List<Memo>
+    suspend fun deleteAnimal(aid: Int): Result<Void>
 
-    fun addMedical(medical: Medical)
+    suspend fun getAnimal(aid: Int): Result<Animal>
 
-    fun addMemo(memo: Memo)
+    suspend fun updateAnimalDetail(aid: Int, animal: Animal): Result<Void>
+
+    suspend fun getMemos(aid: Int): Result<List<Memo>>
+
+    suspend fun setMemo(aid: Int, memo: Memo): Result<Void>
+
+    suspend fun updateMemo(mid: Int, memo: Memo): Result<Void>
+
+    suspend fun deleteMemo(mid: Int): Result<Void>
+
+    suspend fun loadMedical(aid: Int): Result<List<Medical>>
+
+    suspend fun addMedical(aid: Int, medical: Medical): Result<Void>
 
 }
--- mobile/app/src/main/java/com/lacuc/pets/data/animal/DefaultAnimalRepository.kt
+++ mobile/app/src/main/java/com/lacuc/pets/data/animal/DefaultAnimalRepository.kt
@@ -1,5 +1,6 @@
 package com.lacuc.pets.data.animal
 
+import com.lacuc.pets.data.Result
 import com.lacuc.pets.data.animal.entity.Animal
 import com.lacuc.pets.data.animal.entity.Medical
 import com.lacuc.pets.data.animal.entity.Memo
@@ -8,21 +9,41 @@ import javax.inject.Inject
 class DefaultAnimalRepository @Inject constructor(
     private val animalRemoteDataSource: AnimalDataSource
 ) : AnimalRepository {
-    override fun loadAnimal(gid: Int): List<Animal> = animalRemoteDataSource.loadAnimal(gid)
+    override suspend fun getAnimalByGroup(gid: Int): Result<List<Animal>> =
+        animalRemoteDataSource.getAnimalByGroup(gid)
 
-    override fun addAnimal(animal: Animal) {
+    override suspend fun addAnimal(animal: Animal): Result<Void> =
         animalRemoteDataSource.addAnimal(animal)
+
+    override suspend fun loadMedical(aid: Int): Result<List<Medical>> =
+        animalRemoteDataSource.loadMedical(aid)
+
+    override suspend fun getMemos(aid: Int): Result<List<Memo>> =
+        animalRemoteDataSource.getMemos(aid)
+
+    override suspend fun addMedical(aid: Int, medical: Medical): Result<Void> =
+        animalRemoteDataSource.addMedical(aid, medical)
+
+    override suspend fun setMemo(aid: Int, memo: Memo): Result<Void> =
+        animalRemoteDataSource.setMemo(aid, memo)
+
+    override suspend fun deleteAnimal(aid: Int): Result<Void> {
+        TODO("Not yet implemented")
     }
 
-    override fun loadMedical(aid: Int): List<Medical> = animalRemoteDataSource.loadMedical(aid)
-
-    override fun loadMemo(aid: Int): List<Memo> = animalRemoteDataSource.loadMemo(aid)
-
-    override fun addMedical(medical: Medical) {
-        animalRemoteDataSource.addMedical(medical)
+    override suspend fun getAnimal(aid: Int): Result<Animal> {
+        TODO("Not yet implemented")
     }
 
-    override fun addMemo(memo: Memo) {
-        animalRemoteDataSource.addMemo(memo)
+    override suspend fun updateAnimalDetail(aid: Int, animal: Animal): Result<Void> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun updateMemo(mid: Int, memo: Memo): Result<Void> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun deleteMemo(mid: Int): Result<Void> {
+        TODO("Not yet implemented")
     }
 }
