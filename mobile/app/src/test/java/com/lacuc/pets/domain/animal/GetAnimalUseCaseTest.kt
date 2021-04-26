347c8740ad53e73576df1dbeb9efbf6abfcf8be0
SeongHwan Kim <shshksh58@gmail.com>
2021-04-26 21:57:30 +0900

    [test] Animal Test 수정
    - 테스트에서 코루틴 함수를 처리하도록 변경.

--- mobile/app/src/test/java/com/lacuc/pets/domain/animal/GetAnimalUseCaseTest.kt
+++ mobile/app/src/test/java/com/lacuc/pets/domain/animal/GetAnimalUseCaseTest.kt
@@ -1,12 +1,14 @@
 package com.lacuc.pets.domain.animal
 
+import com.lacuc.pets.data.Result
 import com.lacuc.pets.data.animal.AnimalRepository
 import com.lacuc.pets.data.animal.entity.Animal
 import com.lacuc.pets.domain.animal.animal.GetAnimalUseCase
-import org.junit.Assert.assertEquals
-import org.junit.Assert.assertTrue
+import kotlinx.coroutines.runBlocking
+import org.junit.Assert.*
 import org.junit.Before
 import org.junit.Test
+import org.mockito.ArgumentMatchers
 import org.mockito.Mockito.*
 
 class GetAnimalUseCaseTest {
@@ -22,44 +24,63 @@ class GetAnimalUseCaseTest {
     }
 
     @Test
-    fun loadAnimal_empty() {
+    fun loadAnimal_empty() = runBlocking {
+        `when`(repository.getAnimalByGroup(anyInt())).thenReturn(Result.Success(emptyList()))
+
         val result = useCase(0) {}
 
-        assertTrue(result.isEmpty())
-        verify(repository).loadAnimal(anyInt())
+        if (result is Result.Success) {
+            assertNotNull(result.body)
+            assertTrue(result.body?.isEmpty() ?: false)
+            verify(repository).getAnimalByGroup(anyInt())
+        }
     }
 
     @Test
-    fun loadAnimals() {
+    fun loadAnimals() = runBlocking {
         val gid = 1
-        `when`(repository.loadAnimal(gid)).thenReturn(
-            listOf(
-                Animal(gid, "", "", 0, "", "", "", 0.0, ""),
-                Animal(gid, "", "", 0, "", "", "", 0.0, ""),
-                Animal(gid, "", "", 0, "", "", "", 0.0, ""),
+        `when`(repository.getAnimalByGroup(gid)).thenReturn(
+            Result.Success(
+                listOf(
+                    Animal(gid, "", "", 0, "", "", "", 0.0, ""),
+                    Animal(gid, "", "", 0, "", "", "", 0.0, ""),
+                    Animal(gid, "", "", 0, "", "", "", 0.0, ""),
+                )
             )
         )
 
         val result = useCase(1) {}
 
-        assertEquals(result.size, 3)
-        verify(repository).loadAnimal(1)
+        if (result is Result.Success) {
+            assertNotNull(result.body)
+            assertEquals(result.body?.size, 3)
+            verify(repository).getAnimalByGroup(1)
+        }
     }
 
     @Test
-    fun loadAnimals_wrongGid() {
+    fun loadAnimals_wrongGid() = runBlocking {
         val gid = 1
-        `when`(repository.loadAnimal(gid)).thenReturn(
-            listOf(
-                Animal(gid, "", "", 0, "", "", "", 0.0, ""),
-                Animal(gid, "", "", 0, "", "", "", 0.0, ""),
-                Animal(gid, "", "", 0, "", "", "", 0.0, ""),
-            )
-        )
+        `when`(repository.getAnimalByGroup(ArgumentMatchers.anyInt())).thenAnswer{
+            val param = it.arguments[0] as Int
+            if (param == 1)
+                Result.Success(
+                    listOf(
+                        Animal(gid, "", "", 0, "", "", "", 0.0, ""),
+                        Animal(gid, "", "", 0, "", "", "", 0.0, ""),
+                        Animal(gid, "", "", 0, "", "", "", 0.0, ""),
+                    )
+                )
+            else
+                Result.Success(emptyList())
+        }
 
         val result = useCase(0) {}
 
-        assertEquals(result.size, 0)
-        verify(repository).loadAnimal(0)
+        if (result is Result.Success) {
+            assertNotNull(result.body)
+            assertEquals(result.body?.size, 0)
+            verify(repository).getAnimalByGroup(0)
+        }
     }
 }
