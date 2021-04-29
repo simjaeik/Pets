4052b0750c1aed83f49648a4dda70e6c9f288ded
SeongHwan Kim <shshksh58@gmail.com>
2021-04-29 19:51:25 +0900

    [feat] 그룹 관련 엔티티 클래스 추가
    - GroupService에서 사용할 데이터 클래스를 추가로 정의함.

---
+++ mobile/app/src/main/java/com/lacuc/pets/data/group/entity/Member.kt
@@ -0,0 +1,9 @@
+package com.lacuc.pets.data.group.entity
+
+data class Member(
+    val uid: Int,
+    val name: String,
+    val password: String,
+    val email: String,
+    val nickName: String
+)
