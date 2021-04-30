80cd99e104c261bb64bf8c47bf87cfb730972af0
SeongHwan Kim <shshksh58@gmail.com>
2021-04-30 14:31:07 +0900

    [feat] 에러 이벤트 추가
    - 메인 액티비티에 에러 이벤트를 받아서 스낵바로 출력하는 기능을 추가함.

--- mobile/app/src/main/java/com/lacuc/pets/di/AppModule.kt
+++ mobile/app/src/main/java/com/lacuc/pets/di/AppModule.kt
@@ -3,8 +3,10 @@ package com.lacuc.pets.di
 import android.app.Application
 import android.content.Context
 import com.lacuc.pets.App
+import com.lacuc.pets.util.SingleLiveEvent
 import dagger.Binds
 import dagger.Module
+import dagger.Provides
 import javax.inject.Singleton
 
 @Module(includes = [RetrofitModule::class, ViewModelModule::class, RepositoryModule::class])
@@ -19,4 +21,9 @@ abstract class AppModule {
     @ApplicationContext
     abstract fun provideContext(app: App): Context
 
+    companion object {
+        @Provides
+        @Singleton
+        fun provideErrorEvent() = SingleLiveEvent<String>()
+    }
 }
