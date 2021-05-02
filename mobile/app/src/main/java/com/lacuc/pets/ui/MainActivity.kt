ca6bf998ec35e470eb59729b5759206bed953f02
SeongHwan Kim <shshksh58@gmail.com>
2021-05-02 17:10:14 +0900

    [feat] 내비게이션 백스택 로그 설정
    - NavController의 백 스택 변화를 관찰하기 위해 HostFragment의
    childFragmentManager에 백 스택 리스너를 설정함.

--- mobile/app/src/main/java/com/lacuc/pets/ui/MainActivity.kt
+++ mobile/app/src/main/java/com/lacuc/pets/ui/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.lacuc.pets.ui
 
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
 import com.google.android.material.snackbar.Snackbar
+import com.lacuc.pets.R
 import com.lacuc.pets.databinding.ActivityMainBinding
 import com.lacuc.pets.util.SingleLiveEvent
 import dagger.android.support.DaggerAppCompatActivity
+import timber.log.Timber
 import javax.inject.Inject
 
 class MainActivity : DaggerAppCompatActivity() {
@@ -19,7 +22,20 @@ class MainActivity : DaggerAppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        setNavigationBackStackChangeListener()
+
         errorEvent.observe(this, this::showSnackBar)
+    }
+
+    private fun setNavigationBackStackChangeListener() {
+        val childFM =
+            (supportFragmentManager.findFragmentById(R.id.container_navHost) as NavHostFragment).childFragmentManager
+        childFM.addOnBackStackChangedListener {
+            Timber.d("BackStack count: ${childFM.backStackEntryCount}")
+            for (i in 0 until childFM.backStackEntryCount) {
+                Timber.d("BackStack($i): ${childFM.getBackStackEntryAt(i).name}")
+            }
+        }
     }
 
     private fun showSnackBar(message: String) {
