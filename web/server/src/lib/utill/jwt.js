3ca125508169e699a580a61aead0deb2ca8ddf44
jaeik <wodlr2007@naver.com>
2021-05-03 15:53:39 +0900

    [feat] JWT를 검증하는 함수 구현
    - JWT 토큰을 검증하는 함수를 구현함

--- web/server/src/lib/utill/jwt.js
+++ web/server/src/lib/utill/jwt.js
@@ -4,4 +4,12 @@ const { JWT_SECRET } = process.env;
 
 module.exports = {
   createJWT: (id) => jwt.sign({ id }, JWT_SECRET),
+  verifyJWT: (token) => {
+    try {
+      const id = jwt.verify(token, JWT_SECRET);
+      return id;
+    } catch (error) {
+      return false;
+    }
+  },
 };
