43df9d6073d0cb5fb8e18a36cdaa688a97624a8e
jaeik <wodlr2007@naver.com>
2021-05-03 15:54:40 +0900

    [feat] user router에 로그인 라우팅
    - middleware에서 localAuth를 통과하고 controller로 보내는 router 설정

--- web/server/src/routes/user.js
+++ web/server/src/routes/user.js
@@ -1,9 +1,12 @@
 // Dependencies
 const express = require("express");
+const localAuth = require("./middleware/local-auth");
 
 // Controller
-const user = require("../controller/user");
+const userController = require("../controller/user");
 
 const router = express.Router();
+
+router.post("/login", localAuth, userController.login);
 
 module.exports = router;
