97571b5ee660c57db5431ba530d95fb0c1d5eaef
jaeik <wodlr2007@naver.com>
2021-05-05 20:37:49 +0900

    [feat] checkUserValid 함수 구현
    - 회원가입 시 모든 정보가 들어갔는지 확인
    - 이미 존재하는 이메일, 닉네임인지 확인
    - 오류가 있다면 error return, 아니라면 false return

--- web/server/src/service/user.js
+++ web/server/src/service/user.js
@@ -1,4 +1,21 @@
 const { createJWT } = require("../lib/utill/jwt");
+const checkUserValid = async ({ name, password, email, nickName }) => {
+  const existEmail = await Member.findOne({ where: { email } });
+  const existNickName = await Member.findOne({ where: { nickName } });
+
+  if (!name || !password || !email || !nickName) {
+    return { error: "모든 정보를 입력해주세요." };
+  }
+
+  if (existEmail) {
+    return { error: "중복된 email입니다." };
+  }
+
+  if (existNickName) {
+    return { error: "중복된 닉네임입니다." };
+  }
+  return false;
+};
 
 module.exports = {
   login: async (member) => {
