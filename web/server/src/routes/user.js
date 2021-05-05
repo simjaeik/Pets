059081b50ed5da17b1304ffec6c85958bf71fd16
jaeik <wodlr2007@naver.com>
2021-05-05 20:28:25 +0900

    [feat] user router 회원가입 연결
    - signUp을 위한 라우팅 설정

--- web/server/src/routes/user.js
+++ web/server/src/routes/user.js
@@ -7,6 +7,8 @@ const userController = require("../controller/user");
 
 const router = express.Router();
 
+router.post("/signUp", userController.signUp);
+
 router.post("/login", localAuth, userController.login);
 
 module.exports = router;
