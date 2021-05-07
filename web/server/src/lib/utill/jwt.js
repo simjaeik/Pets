e58954373c7ee01c9ee51e3ca2f26997acedf4e6
JaeIk <43948654+simjaeik@users.noreply.github.com>
2021-05-07 23:03:17 +0900

    [fix] JWT create 함수 구조 변경
    - create 함수의 매개변수를 변경했다.

--- web/server/src/lib/utill/jwt.js
+++ web/server/src/lib/utill/jwt.js
@@ -3,8 +3,13 @@ const jwt = require("jsonwebtoken");
 const { JWT_SECRET } = process.env;
 
 module.exports = {
-  createJWT: (id) =>
-    jwt.sign({ id, exp: Math.floor(Date.now() / 1000) + 60 * 60 }, JWT_SECRET),
+  createJWT: (data) => {
+    return jwt.sign(
+      { data, exp: Math.floor(Date.now() / 1000) + 60 * 60 },
+      JWT_SECRET
+    );
+  },
+
   verifyJWT: (token) => {
     try {
       const id = jwt.verify(token, JWT_SECRET);
