523e7c4fc2863080d1cb63654a26e400393b8250
JaeIk <43948654+simjaeik@users.noreply.github.com>
2021-05-11 16:50:19 +0900

    [feat] getMyGroups route 설정
    - getMyGroups를 위한 라우터 설정

--- web/server/src/routes/group.js
+++ web/server/src/routes/group.js
@@ -2,8 +2,10 @@
 const express = require("express");
 
 // Controller
-const group = require("../controller/group");
+const controller = require("../controller/group");
 
 const router = express.Router();
+
+router.get("/", controller.getMyGoups);
 
 module.exports = router;
