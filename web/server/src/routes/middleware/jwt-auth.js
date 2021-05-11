f42bf3f1e4818a8a1f4aff082c29db953d830947
JaeIk <43948654+simjaeik@users.noreply.github.com>
2021-05-11 16:46:39 +0900

    [fix] jwt-auth 매개변수 변경
    - jwt-auth의 받는 데이터, 매개변수를 변경하고 verifyJWT에 넣을 변수를 수정해주었다.

--- web/server/src/routes/middleware/jwt-auth.js
+++ web/server/src/routes/middleware/jwt-auth.js
@@ -4,7 +4,10 @@ const { verifyJWT } = require("../../lib/utill/jwt");
 
 module.exports = (req, res, next) => {
   passport.authenticate("jwt", { session: false }, (error, user) => {
-    if (!user || verifyJWT(user.id)) {
+    const { authorization } = req.headers;
+    const { data } = verifyJWT(authorization);
+
+    if (!data) {
       return res.status(401).json("로그인 해주시기 바랍니다");
     }
     if (error) {
@@ -12,7 +15,7 @@ module.exports = (req, res, next) => {
     }
 
     // 권한 등 체크 가능
-    req.user = user;
+    req.data = data;
     return next();
   })(req, res, next);
 };
