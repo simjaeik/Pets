aa24ccd9e246a3dfb9fd43597e7a010e33109abf
SeongHwan Kim <shshksh58@gmail.com>
2021-05-11 14:36:18 +0900

    [feat] ID 프로퍼티 추가
    - ManageViewModel에 각각의 item의 id를 관리하는 프로퍼티를 추가함.

--- mobile/app/src/main/java/com/lacuc/pets/ui/manage/ManageViewModel.kt
+++ mobile/app/src/main/java/com/lacuc/pets/ui/manage/ManageViewModel.kt
@@ -3,4 +3,9 @@ package com.lacuc.pets.ui.manage
 import androidx.lifecycle.ViewModel
 
 class ManageViewModel: ViewModel() {
+    var gid: Int? = null
+    var aid: Int? = null
+    var iid: Int? = null
+    var uid: Int? = null
+    var hid: Int? = null
 }
