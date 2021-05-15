19fd305d3137f0867217a2f2aea31cb0a624b27f
SeongHwan Kim <shshksh58@gmail.com>
2021-05-15 20:21:14 +0900

    [feat] updateGroup 유스케이스 수정
    - 이미지 uri의 패턴에 따라 멀티파트를 생성하는 로직을 분기함.

--- mobile/app/src/main/java/com/lacuc/pets/domain/group/UpdateGroupUseCase.kt
+++ mobile/app/src/main/java/com/lacuc/pets/domain/group/UpdateGroupUseCase.kt
@@ -2,11 +2,37 @@ package com.lacuc.pets.domain.group
 
 import com.lacuc.pets.data.Result
 import com.lacuc.pets.data.group.GroupRepository
-import com.lacuc.pets.data.group.entity.Group
+import okhttp3.MediaType.Companion.toMediaTypeOrNull
+import okhttp3.MultipartBody
+import okhttp3.RequestBody.Companion.asRequestBody
+import java.io.File
+import java.util.regex.Pattern
 import javax.inject.Inject
 
 class UpdateGroupUseCase @Inject constructor(private val repository: GroupRepository) {
-    suspend operator fun invoke(group: Group): Result<Void> {
-        return repository.updateGroup(group)
+    suspend operator fun invoke(
+        gid: String,
+        name: String,
+        info: String,
+        imageUri: String,
+        share: Boolean
+    ): Result<Void> {
+        val partMap = mapOf(
+            "name" to name,
+            "info" to info,
+            "share" to share.toString(),
+            "latitude" to "0",
+            "longitude" to "0"
+        )
+        return if (Pattern.matches("^(http)", imageUri)) {
+            val imagePart = MultipartBody.Part.createFormData("image", imageUri)
+            repository.updateGroup(gid, partMap, imagePart)
+        } else {
+            val imageFile = File(imageUri)
+            val imageBody = imageFile.asRequestBody("image/*".toMediaTypeOrNull())
+            val imagePart = MultipartBody.Part.createFormData("image", imageFile.name, imageBody)
+
+            repository.updateGroup(gid, partMap, imagePart)
+        }
     }
 }
