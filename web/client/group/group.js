6c97f5409f078535d25011bfd0e4f66f061e11bf
hyerimseong <shl7810@naver.com>
2021-05-17 00:36:46 +0900

    [feat] 그룹 정보 수정

--- web/client/group/group.js
+++ web/client/group/group.js
@@ -84,67 +84,144 @@ function toggleShare(){
 // .catch(error => {
 //     console.log(error.response)
 // }); 
-// function modal() {
+function modal() {
 
-//     const zIndex = 9999;   
-//     const bg = document.createElement('div');
-//     const modal = document.getElementById('my_modal');
+    const zIndex = 9999;   
+    const bg = document.createElement('div');
+    const modal = document.getElementById('my_modal');
     
-//     // 모달 div 뒤에 희끄무레한 레이어
-//     bg.setStyle({
-//         position: 'fixed',
-//         zIndex: zIndex,
-//         left: '0px',
-//         top: '0px',
-//         width: '100%',
-//         height: '100%',
-//         overflow: 'auto',
-//         // 레이어 색갈은 여기서 바꾸면 됨
-//         backgroundColor: 'rgba(0,0,0,0.4)'
-//     });
-//     document.body.append(bg);
+    // 모달 div 뒤에 희끄무레한 레이어
+    bg.setStyle({
+        position: 'fixed',
+        zIndex: zIndex,
+        left: '0px',
+        top: '0px',
+        width: '100%',
+        height: '100%',
+        overflow: 'auto',
+        // 레이어 색갈은 여기서 바꾸면 됨
+        backgroundColor: 'rgba(0,0,0,0.4)'
+    });
+    document.body.append(bg);
 
-//     // 닫기 버튼 처리, 시꺼먼 레이어와 모달 div 지우기
-//     modal.querySelector('.modal_btn').addEventListener('click', function() {
-//         bg.remove();
-//         modal.style.display = 'none';
-//     });
+    // 닫기 버튼 처리, 시꺼먼 레이어와 모달 div 지우기
+    modal.querySelector('.modal_btn').addEventListener('click', function() {
+        bg.remove();
+        modal.style.display = 'none';
+    });
 
-//     modal.querySelector('#addgroup_btn').addEventListener('click', function() {
+    modal.querySelector('#addgroup_btn').addEventListener('click', function() {
 
-//         bg.remove();
-//         modal.style.display = 'none';
-//     });
+        bg.remove();
+        modal.style.display = 'none';
+    });
 
-//     modal.setStyle({
-//         position: 'fixed',
-//         display: 'block',
-//         boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)',
+    modal.setStyle({
+        position: 'fixed',
+        display: 'block',
+        boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)',
 
-//         // 시꺼먼 레이어 보다 한칸 위에 보이기
-//         zIndex: zIndex + 1,
-//         // div center 정렬
-//         top: '50%',
-//         left: '50%',
-//         transform: 'translate(-50%, -50%)',
-//         msTransform: 'translate(-50%, -50%)',
-//         webkitTransform: 'translate(-50%, -50%)'
-//     });
+        // 시꺼먼 레이어 보다 한칸 위에 보이기
+        zIndex: zIndex + 1,
+        // div center 정렬
+        top: '50%',
+        left: '50%',
+        transform: 'translate(-50%, -50%)',
+        msTransform: 'translate(-50%, -50%)',
+        webkitTransform: 'translate(-50%, -50%)'
+    });
 
-// }
-// // Element 에 style 한번에 오브젝트로 설정하는 함수 추가
-// Element.prototype.setStyle = function(styles) {
-//     for (var k in styles) this.style[k] = styles[k];
-//     return this;
-// };
-// function initinput(){
+}
+function updategroup_modal() {
 
-//     const input = document.getElementsByClassName('input');
+    const zIndex = 9999;   
+    const bg = document.createElement('div');
+    const modal = document.getElementById('updategroup');
     
-//     for(let i=0;i<input.length;i++){
-//         input[i].value= "";
-//     }
-// }
+    // 모달 div 뒤에 희끄무레한 레이어
+    bg.setStyle({
+        position: 'fixed',
+        zIndex: zIndex,
+        left: '0px',
+        top: '0px',
+        width: '100%',
+        height: '100%',
+        overflow: 'auto',
+        // 레이어 색갈은 여기서 바꾸면 됨
+        backgroundColor: 'rgba(0,0,0,0.4)'
+    });
+    document.body.append(bg);
+
+    // 닫기 버튼 처리, 시꺼먼 레이어와 모달 div 지우기
+    modal.querySelector('.modal_btn').addEventListener('click', function() {
+        bg.remove();
+        modal.style.display = 'none';
+    });
+
+    modal.querySelector('#updategroup_btn').addEventListener('click', function() {
+
+        const updatename = document.querySelector("#updatename").value;
+        const updateinfo = document.querySelector("#updateinfo").value;
+        const updatelat = document.querySelector("#updatelat").value;
+        const updatelon = document.querySelector("#updatelon").value;        
+
+        if(updatename === "") { alert("바꿀 그룹 이름을 입력해주세요. "); }
+        else if(updateinfo === "") { alert("바꿀 그룹 소개를 입력해주세요. "); }
+        else if(parseInt(updatelat) === null || parseInt(updatelon) === null) { alert("바꿀 위치를 입력해주세요.") }
+        else{
+            bg.remove();
+            modal.style.display = 'none';
+
+            axios.patch(`${URL}/group/${GID}`,{ 
+                name: updatename,
+                info: updateinfo,
+                image: "aabbccdd",
+                latitude: parseInt(updatelat),
+                longitude: parseInt(updatelon)
+            },{
+                headers : {
+                    'authorization' : jwtToken
+                }
+            })
+            .then(response => {
+                console.log(response)
+                alert("성공적으로 수정되었습니다.");
+            })
+            .catch(error => {
+                console.log(error.response)
+            }); 
+        }
+    });
+
+    modal.setStyle({
+        position: 'fixed',
+        display: 'block',
+        boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)',
+
+        // 시꺼먼 레이어 보다 한칸 위에 보이기
+        zIndex: zIndex + 1,
+        // div center 정렬
+        top: '50%',
+        left: '50%',
+        transform: 'translate(-50%, -50%)',
+        msTransform: 'translate(-50%, -50%)',
+        webkitTransform: 'translate(-50%, -50%)'
+    });
+
+}
+// Element 에 style 한번에 오브젝트로 설정하는 함수 추가
+Element.prototype.setStyle = function(styles) {
+    for (var k in styles) this.style[k] = styles[k];
+    return this;
+};
+function initinput(){
+
+    const input = document.getElementsByClassName('input');
+    
+    for(let i=0;i<input.length;i++){
+        input[i].value= "";
+    }
+}
 // function addGroup(event){
 
 //     const nameinputValue = document.querySelector("#nameinput").value;
