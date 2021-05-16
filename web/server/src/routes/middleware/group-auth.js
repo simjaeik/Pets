b54e6168ba8c25d5d26641fbaa16d830d6ea78ce
JaeIk <43948654+simjaeik@users.noreply.github.com>
2021-05-16 17:30:47 +0900

    [fix] group-auth 예외처리 추가
    - GID & UID가 입력이 잘못 된 경우를 예외처리해줌

--- web/server/src/routes/middleware/group-auth.js
+++ web/server/src/routes/middleware/group-auth.js
@@ -13,18 +13,29 @@ module.exports = async (req, res, next) => {
     return res.status(401).json("로그인 해주시기 바랍니다");
   }
 
-  const { authority } = await MemberGroup.findOne({
-    where: {
-      [Op.and]: [{ UID }, { GID: id }],
-    },
-    attributes: ["authority"],
-    raw: true,
-  });
+  try {
+    const memberGroup = await MemberGroup.findOne({
+      where: {
+        [Op.and]: [{ UID }, { GID: id }],
+      },
+      attributes: ["authority"],
+      raw: true,
+    });
 
-  if (authority === "수정권한" || authority === "관리자") {
-    req.auth = authority;
-    req.gid = id;
-    return next();
+    if (memberGroup === null || memberGroup === undefined) {
+      return res.status(400).json("해당 그룹 & 멤버가 존재하지 않습니다.");
+    }
+
+    const { authority } = memberGroup;
+    if (authority === "수정권한" || authority === "관리자") {
+      req.auth = authority;
+      req.gid = id;
+      req.uid = UID;
+      return next();
+    }
+  } catch (error) {
+    console.log(error);
+    return { error };
   }
 
   return res.status(401).json("이 그룹에 권한이 없습니다.");
