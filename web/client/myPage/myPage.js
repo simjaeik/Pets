00a3e6fa158b7831ef6ab28766b709dcfcd556ce
hyerimseong <shl7810@naver.com>
2021-05-17 00:50:53 +0900

    [feat] 로그인한 유저 정보 조회

--- web/client/myPage/myPage.js
+++ web/client/myPage/myPage.js
@@ -1,36 +1,32 @@
 const URL = "http://ec2-54-180-91-27.ap-northeast-2.compute.amazonaws.com:3000/api";
 const jwtToken = sessionStorage.getItem("jwt");
 const exist=0;
-// axios.get(`${URL}/user`,
-// { 
-//     headers : {
-//         'authorization' : jwtToken
-//     }
-// })
-// .then(response => {
-//     console.log(response)
+axios.get(`${URL}/user`,
+{ 
+    headers : {
+        'authorization' : jwtToken
+    }
+})
+.then(response => {
+    console.log(response)
+    console.log(response.data.nickname);
+    const userNickname = response.data.nickname;
+    const username = response.data.name;
+    const useremail = response.data.email;
+
+    const nickName = document.getElementById("usernickname");
+    nickName.innerText = userNickname;
+
+    const Name = document.getElementById("username");
+    Name.innerText = username;
+
+    const Email = document.getElementById("useremail");
+    Email.innerText = useremail;
  
-//     // {
-//     //     name : 이름,
-//     //     email : 이메일,
-//     //     nickName : 유저 닉네임
-//     // }
-// })
-// .catch(error => {
-//     console.log(error.response)
-// }); 
-
-const userNickname = "micky";
-const username = "mouse"; const useremail="micky@naver.com";
-
-const nickName = document.getElementById("usernickname");
-nickName.innerText = userNickname;
-
-const Name = document.getElementById("username");
-Name.innerText = username;
-
-const Email = document.getElementById("useremail");
-Email.innerText = useremail;
+})
+.catch(error => {
+    console.log(error.response)
+}); 
 
 function modal() {
 
