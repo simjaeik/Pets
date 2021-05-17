3c9d2e4a9db1111d8c4ecd4174fdcdd887802c49
JaeIk <43948654+simjaeik@users.noreply.github.com>
2021-05-18 02:21:49 +0900

    [feat] getGalleryImage 서비스 로직 구현
    - IID를 받아 세부정보를 주는 서비스 로직 구현

--- web/server/src/service/image.js
+++ web/server/src/service/image.js
@@ -17,4 +17,22 @@ module.exports = {
     }
   },
 
+  getGalleryImage: async ({ id }) => {
+    if (!id) {
+      return { error: "모든 정보를 입력해주세요" };
+    }
+    try {
+      const image = await GalleryImage.findOne({
+        where: { IID: id },
+      });
+      if (image <= 0) {
+        return { result: false, error: "조회된 데이터가 없습니다." };
+      }
+      return image;
+    } catch (error) {
+      console.log(error);
+      return { result: false, error };
+    }
+  },
+
 };
