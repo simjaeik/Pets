975ddda6f69875984b0191538ce6f0fa4201a133
SeongHwan Kim <shshksh58@gmail.com>
2021-05-17 21:09:02 +0900

    [refactor] MedicalItem 리스너 추가

--- mobile/app/src/main/java/com/lacuc/pets/domain/animal/medical/GetMedicalsUseCase.kt
+++ mobile/app/src/main/java/com/lacuc/pets/domain/animal/medical/GetMedicalsUseCase.kt
@@ -5,10 +5,14 @@ import com.lacuc.pets.data.animal.AnimalRepository
 import com.lacuc.pets.domain.animal.AnimalDetailMedicalItem
 import javax.inject.Inject
 
-class GetMedicalUseCase @Inject constructor(private val repository: AnimalRepository) {
-    suspend operator fun invoke(aid: String): Result<List<AnimalDetailMedicalItem>> {
+class GetMedicalsUseCase @Inject constructor(private val repository: AnimalRepository) {
+    suspend operator fun invoke(
+        aid: String,
+        listener: (AnimalDetailMedicalItem) -> Unit
+    ): Result<List<AnimalDetailMedicalItem>> {
         return when (val medicals = repository.loadMedical(aid)) {
-            is Result.Success -> Result.Success(medicals.body?.map { AnimalDetailMedicalItem(it) })
+            is Result.Success ->
+                Result.Success(medicals.body?.map { AnimalDetailMedicalItem(it, listener) })
             is Result.Failure -> medicals
             is Result.NetworkError -> medicals
             is Result.Unexpected -> medicals
