6b7eef9011872f84b5bfe3fb682465951a9e5bbc
SeongHwan Kim <shshksh58@gmail.com>
2021-05-17 13:10:49 +0900

    [feat] 멤버 추가 함수 구현

--- mobile/app/src/main/java/com/lacuc/pets/domain/member/InviteMemberUseCase.kt
+++ mobile/app/src/main/java/com/lacuc/pets/domain/member/InviteMemberUseCase.kt
@@ -5,7 +5,11 @@ import com.lacuc.pets.data.group.GroupRepository
 import javax.inject.Inject
 
 class InviteMemberUseCase @Inject constructor(private val repository: GroupRepository) {
-    suspend operator fun invoke(gid: String, email: String, authority: String): Result<Void> {
+    suspend operator fun invoke(
+        gid: String,
+        email: String,
+        authority: String = "수정권한"
+    ): Result<Void> {
         return repository.addGroupMember(gid, email, authority)
     }
 }
--- mobile/app/src/main/java/com/lacuc/pets/ui/manage/group/member/invite/InviteMemberViewModel.kt
+++ mobile/app/src/main/java/com/lacuc/pets/ui/manage/group/member/invite/InviteMemberViewModel.kt
@@ -1,6 +1,42 @@
 package com.lacuc.pets.ui.manage.group.member.invite
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.lacuc.pets.data.Result
+import com.lacuc.pets.domain.member.InviteMemberUseCase
+import com.lacuc.pets.util.SingleLiveEvent
+import com.lacuc.pets.util.safeValue
+import kotlinx.coroutines.launch
+import timber.log.Timber
+import javax.inject.Inject
 
-class InviteMemberViewModel : ViewModel() {
+class InviteMemberViewModel @Inject constructor(
+    private val inviteMemberUseCase: InviteMemberUseCase,
+    private val errorEvent: SingleLiveEvent<String>
+) : ViewModel() {
+
+    var gid = ""
+
+    val email = MutableLiveData("")
+
+    val completeEvent = SingleLiveEvent<Unit>()
+
+    fun inviteMember() {
+        viewModelScope.launch {
+            val result = inviteMemberUseCase(gid, email.safeValue)
+
+            when (result) {
+                is Result.Success -> result.body?.let { completeEvent.value = Unit }
+                is Result.Failure -> errorEvent.value =
+                    "code: ${result.code} message: ${result.error}"
+                is Result.NetworkError -> errorEvent.value = "네트워크 문제가 발생했습니다."
+                is Result.Unexpected -> {
+                    Timber.d(result.t.toString())
+                    errorEvent.value = "알수없는 오류가 발생했습니다."
+                }
+            }
+        }
+    }
+
 }
