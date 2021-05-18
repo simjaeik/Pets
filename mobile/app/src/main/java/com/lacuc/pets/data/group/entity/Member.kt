4797043ba3e38e07e2810645a5fc671d84b57ef2
SeongHwan Kim <shshksh58@gmail.com>
2021-05-18 17:09:39 +0900

    [feat] Member 클래스 SerializedName 어노테이션 추가

--- mobile/app/src/main/java/com/lacuc/pets/data/group/entity/Member.kt
+++ mobile/app/src/main/java/com/lacuc/pets/data/group/entity/Member.kt
@@ -1,10 +1,12 @@
 package com.lacuc.pets.data.group.entity
 
+import com.google.gson.annotations.SerializedName
+
 data class Member(
     val UID: String,
-    val name: String,
+    @SerializedName("Member.name") val name: String,
     val password: String,
-    val email: String,
-    val nickname: String,
+    @SerializedName("Member.email") val email: String,
+    @SerializedName("Member.nickName") val nickname: String,
     val authority: String = "권한없음"
 )
